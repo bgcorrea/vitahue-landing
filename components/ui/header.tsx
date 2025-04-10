83209dec3522663862a23b8058844d07fa075218
bgcorrea <bgcorreap@gmail.com>
2025-04-09 20:46:49 -0400

    Eliminación de botones de login y registro del header

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -10,26 +10,6 @@ export default function Header() {
           <div className="flex flex-1 items-center">
             <Logo />
           </div>
-
-          {/* Desktop sign in links */}
-          <ul className="flex flex-1 items-center justify-end gap-3">
-            <li>
-              <Link
-                href="/signin"
-                className="btn-sm bg-white text-gray-800 shadow-sm hover:bg-gray-50"
-              >
-                Login
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/signup"
-                className="btn-sm bg-gray-800 text-gray-200 shadow-sm hover:bg-gray-900"
-              >
-                Register
-              </Link>
-            </li>
-          </ul>
         </div>
       </div>
     </header>
